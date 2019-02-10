8a5de3ff4382bfb4229d7877356db2090b95b7b9
jeffgietz <jeff@myadbox.com.au>
2019-02-11 10:33:33 +1100

    Added Y axis validation

--- src/components/PlaceForm/PlaceForm.js
+++ src/components/PlaceForm/PlaceForm.js
@@ -1,98 +1,91 @@
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
-import { Field, Formik } from 'formik'
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import { Field, Formik } from "formik";
 
-import './PlaceForm.css'
+import "./PlaceForm.css";
 
 class PlaceForm extends PureComponent {
-  render () {
+  render() {
     return (
       <Formik
-        initialValues={{ posX: '', posY: '', rotation: '0' }}
-        onSubmit={(values) => {
-          this.props.handleSubmit(values)
+        initialValues={{ posX: "", posY: "", rotation: "0" }}
+        onSubmit={values => {
+          this.props.handleSubmit(values);
         }}
         validate={values => {
           let errors = {};
-          const { posX, posY } = values
+          const { posX, posY } = values;
           if (posX !== 0 && !posX) {
-          errors.posX = 'Required';
+            errors.posX = "Required";
           } else if (!/^[0-9]+$/.test(posX)) {
-            errors.posX = 'Please only use numbers';
-          } else if (
-            posX > this.props.maxX
-          ) {
-            errors.posX = 'Placement outside bounds of grid'
+            errors.posX = "Please only use numbers";
+          } else if (posX > this.props.maxX) {
+            errors.posX = "X placement outside bounds of grid";
           }
           if (posY !== 0 && !posY) {
-            errors.posY = 'Required';
-          } else if (
-            !/^[0-9]+$/.test(posY)
-          ) {
-            errors.posY = 'Please only use numbers';
+            errors.posY = "Required";
+          } else if (!/^[0-9]+$/.test(posY)) {
+            errors.posY = "Please only use numbers";
+          } else if (posY > this.props.maxY) {
+            errors.posY = "Y placement outside bounds of grid";
           }
           return errors;
         }}
       >
-          {({
-            errors,
-            handleSubmit,
-          }) => (
-            <form
-              className={`placeform ${this.props.visible ? 'visible' : 'hidden'}`}
-              onSubmit={handleSubmit}
-            >
-              <Field
-                name='posX'
-                render={({ field }) => (
-                  <div className='field'>
-                    <label htmlFor='posX'>Place Robot X:</label>    
-                    <input {...field} placeholder='Position X' />
-                    {errors.posX && <div className="error">{errors.posX}</div>}
-                  </div>
-                )}
-              />
-              <Field
-                name='posY'
-                render={({ field }) => (
-                  <div className='field'>
-                    <label htmlFor='posY'>Place Robot Y:</label>
-                    <input {...field} placeholder='Position Y' />
-                    {errors.posY && <div className="error">{errors.posY}</div>}
-                  </div>
-                )}
-              />
-              <Field
-                name='rotation'
-                render={({ field }) => (
-                  <div className='field'>
-                    <label htmlFor='rotation'>Direction:</label>    
-                    <select name='rotation' {...field}>
-                      <option value='0'>North</option>                      
-                      <option value="90">East</option>
-                      <option value="180">South</option>
-                      <option value="270">West</option>
-                    </select>
-                  </div>
-                )}
-              />
-              <button type="submit">
-                Submit
-              </button>
-            </form>
-          )}
-        </Formik>
-    )
+        {({ errors, handleSubmit }) => (
+          <form
+            className={`placeform ${this.props.visible ? "visible" : "hidden"}`}
+            onSubmit={handleSubmit}
+          >
+            <Field
+              name="posX"
+              render={({ field }) => (
+                <div className="field">
+                  <label htmlFor="posX">Place Robot X:</label>
+                  <input {...field} placeholder="Position X" />
+                  {errors.posX && <div className="error">{errors.posX}</div>}
+                </div>
+              )}
+            />
+            <Field
+              name="posY"
+              render={({ field }) => (
+                <div className="field">
+                  <label htmlFor="posY">Place Robot Y:</label>
+                  <input {...field} placeholder="Position Y" />
+                  {errors.posY && <div className="error">{errors.posY}</div>}
+                </div>
+              )}
+            />
+            <Field
+              name="rotation"
+              render={({ field }) => (
+                <div className="field">
+                  <label htmlFor="rotation">Direction:</label>
+                  <select name="rotation" {...field}>
+                    <option value="0">North</option>
+                    <option value="90">East</option>
+                    <option value="180">South</option>
+                    <option value="270">West</option>
+                  </select>
+                </div>
+              )}
+            />
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </Formik>
+    );
   }
 }
 
 PlaceForm.defaultProps = {
   visible: true
-}
+};
 
 PlaceForm.propTypes = {
   handleSubmit: PropTypes.func,
   visible: PropTypes.bool
-}
+};
 
-export default PlaceForm
+export default PlaceForm;
